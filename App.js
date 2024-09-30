7545113a35c7cce084ca7a63283ae7757a54d2e1
SadiasHossain <158283811+SadiasHossain@users.noreply.github.com>
2024-09-30 16:10:23 +0100

    step 2 completed

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import Logo from './components/logo';
+import React, {useState} from 'react';
+
+
 
 export default function App(){
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
+
   return(
     <View>
       <Logo/>
+      <TextInput placeholder="Enter your firstname" onChangeText={(val) => setFname(val)}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={(val) => setLname(val)}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={(val) => setDob(val)}/>
+      <Text>Hello {fname} {lname}. You were born on {dob}</Text>
     </View>  
   )
-}
-
 
